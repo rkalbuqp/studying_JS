343fe181cfa022f1e57bc07f6ff16d73c5f3d4b1
Kassia Albuquerque <kassiademaria@gmail.com>
2024-08-03 16:42:01 -0300

    lista de exercício 1-30

---
+++ index.js
@@ -0,0 +1,175 @@
+// exercício 1
+
+console.log("Let's build a pyramid using JavaScript basics");
+console.log("    #   ");
+console.log("   ###   ");
+console.log("  ##### ");
+console.log(" ####### ");
+console.log("#########");
+
+// exercício 2
+let character;
+
+//exercício 3
+let character = "Hello";
+
+//exercício 4
+let character = 'Hello';
+
+//exercício 5
+let character = 'Hello';
+console.log(character);
+
+//exercício 6
+let character = 'Hello';
+console.log(character);
+character = "World";
+
+//exercício 7
+let character = 'Hello';
+console.log(character);
+character = "World";
+
+console.log(character);
+
+//exercício 8
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+
+let secondCharacter;
+secondCharacter = 'Hi';
+console.log(secondCharacter);
+
+//exercício 9
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
+
+//exercício 10
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
+
+//exercício 11
+secondCharacter = "Test";
+character = "World";
+
+secondCharacter = character;
+
+//exercício 12
+let character = 'Hello';
+
+//exercício 13
+let profession = 'teacher';
+let age;
+
+console.log(profession);
+console.log(age);
+
+//exercício 14
+let character = "Hello";
+
+//exercício 15
+let character = 'Hello';
+let count = 8;
+
+//exercício 16
+let character = 'Hello';
+let count = 8;
+console.log(count + 1);
+
+//exercpicio 17
+let count = 8;
+console.log(count + 1);
+console.log(count * 32);
+console.log(count - 2);
+console.log(count / 2); 
+// mas precisa remover, então:
+let count = 8;
+
+//exercício 18
+let character = 'Hello';
+let count = 8;
+let rows = [];
+
+//exercício 19
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+
+//exercício 20
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+//exercício 21
+rows[2] = 10
+
+console.log(rows);
+
+//exercício 22
+rows[rows.length - 1] = 10;
+console.log(rows);
+
+//exercício 23
+let rows;
+
+//exercício 24
+let cities = ['London', 'New York', 'Mumbai'];
+console.log(cities);
+
+cities[cities.length - 1] = 'Mexico City';
+console.log(cities);
+
+//exercício 25
+Apenas remover o código
+
+//exercício 26
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+console.log(rows);
+
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+rows[rows.length - 1] = 10;
+rows.push('freeCodeCamp');
+
+console.log(rows);
+
+//exercício 27
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+
+console.log(rows);
+
+let popped = rows.pop();
+
+console.log(rows);
+console.log(popped);
+
+//exercício 28
+rows.push("freeCodeCamp");
+let pushed = rows.push();
+console.log(pushed);
+
+//exercício 29
+let fruits = ['Apple', 'Banana'];
+let newLength = fruits.push('Cherry'); // Adiciona 'Cherry' e retorna o comprimento atualizado, que é 3
+console.log(newLength); // Exibe 3
+
+let lastFruit = fruits.pop(); // Remove 'Cherry' e retorna 'Cherry'
+console.log(lastFruit); // Exibe 'Cherry'
+console.log(fruits); // Exibe ['Apple', 'Banana']
+
+
+//exercício 30
+let character = "Hello";
+let count = 8;
+let rows = [];
